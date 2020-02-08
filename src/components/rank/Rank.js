fb1f71c8587fb9ac1d2738d4604c65a7efb8dc5c
Ricky Gonzalez <rickygonzalez@gmail.com>
2020-02-08 15:50:55 -0600

    prettier on signin.js

--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function Rank( {userName, entries}) {
-    return (
-        <div className='white f3'>
-            <div>{userName}, you have {entries} entries..</div>
-            <div className='white f1'>#5</div>
-            
-        </div>
-    )
+function Rank({ userName, entries }) {
+  return (
+    <div className="white f3">
+      <div>
+        {userName}, you have {entries} entries..
+      </div>
+      <div className="white f1">#5</div>
+    </div>
+  );
 }
 
-export default Rank
+export default Rank;
